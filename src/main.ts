b6d6d379e2abb60c953e74574bc4ec94f11e889e
longlevan9x <longvan1296@gmail.com>
2023-06-16 19:42:53 +0700

    book

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,15 @@ async function bootstrap() {
 
   app.useGlobalPipes(new ValidationPipe());
 
-  app.enableCors({ origin: false });
+  app.enableCors({
+    origin: function (origin, callback) {
+      callback(null, true);
+      // if (!origin || whitelist.indexOf(origin) !== -1) {
+      // } else {
+      //   callback(new Error('Not allowed by CORS'))
+      // }
+    },
+  });
 
   await app.listen(3000);
 }
