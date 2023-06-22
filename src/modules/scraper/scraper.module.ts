0e6d3af986ad6a328ac26dbb495c347953cdd8e1
longlevan9x <longvan1296@gmail.com>
2023-06-22 18:43:49 +0700

    grammar, change vocabulary

--- src/modules/scraper/scraper.module.ts
+++ src/modules/scraper/scraper.module.ts
@@ -1,11 +1,10 @@
 import { Module } from '@nestjs/common';
 import { ScraperService } from './scraper.service';
 import { ScraperController } from './scraper.controller';
-import { PuppeteerService } from 'src/app/services/puppeteer.service';
 import { ScrapingServiceInterface } from '../../app/services/scraping.service';
 
 @Module({
   controllers: [ScraperController],
-  providers: [ScraperService, PuppeteerService, ScrapingServiceInterface],
+  providers: [ScraperService, ScrapingServiceInterface],
 })
 export class ScraperModule {}
